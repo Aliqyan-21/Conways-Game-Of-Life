1a2fb123e73165310556a790ba21091c4864e657
aliqyan-21 <aliqyanabid21@gmail.com>
2024-02-27 22:41:50 +0530

    intialization and display

--- main.cpp
+++ main.cpp
@@ -1,26 +1,40 @@
 #include <ctime>
 #include <iostream>
 
-#define Width 10
-#define Height 10
+#define Width 100
+#define Height 100
+
+// to visualize - density of pixels!
+char level[] = " .-=coaA@#";
+#define level_count (sizeof(level) / sizeof(char)) - 1
 
 float grid[Height][Width] = {0.f};
+float ra = 21;
 
 inline float rand_float() { return (float)rand() / (float)RAND_MAX; }
 
-int main() {
-  srand(time(0));
+void random_grid() {
   for (size_t i = 0; i < Height; i++) {
     for (size_t j = 0; j < Width; j++) {
       grid[i][j] = rand_float();
     }
   }
+}
 
+void display_grid() {
   for (auto &val : grid) {
     for (float &i : val) {
-      std::cout << i << " ";
+      char ch = level[(int)(i * ((int)level_count - 1))];
+      std::cout << ch;
     }
     std::cout << std::endl;
   }
+}
+
+int main() {
+  srand(time(0));
+  random_grid();
+  display_grid();
+
   return 0;
 }
