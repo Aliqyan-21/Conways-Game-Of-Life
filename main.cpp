7914ae4a6dd793f401af5b6117895ee89244784e
aliqyan-21 <aliqyanabid21@gmail.com>
2024-02-28 20:10:57 +0530

    calculating m and n according to the paper

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ char level[] = " .-=coaA@#";
 
 float grid[Height][Width] = {0.f};
 float ra = 21;
+float ri = ra / 3;
 
 inline float rand_float() { return (float)rand() / (float)RAND_MAX; }
 
@@ -31,10 +32,37 @@ void display_grid() {
   }
 }
 
+inline int emod(int a, int b) { return (a % b + b) % b; }
+
 int main() {
   srand(time(0));
   random_grid();
-  display_grid();
+
+  // points, and m & n
+  int cx = 0, cy = 0;
+  float m = 0, M = 0;
+  float n = 0, N = 0;
+
+  for (int dy = -(ra - 1); dy < (ra - 1); dy++) {
+    for (int dx = -(ra - 1); dx < (ra - 1); dx++) {
+      int x = emod(cx + dx, Width);
+      int y = emod(cy + dy, Height);
+
+      if (dx * dx + dy * dy <= ri * ri) {
+        m += grid[y][x];
+        M++;
+      } else if (dx * dx + dy * dy <= ra * ra) {
+        n += grid[y][x];
+        N++;
+      }
+    }
+  }
+  m /= M;
+  n /= N;
+
+  std::cout << "m = " << m << " n = " << n << std::endl;
+
+  // display_grid();
 
   return 0;
 }
