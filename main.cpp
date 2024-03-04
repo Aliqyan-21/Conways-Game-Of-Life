7997e2295f0966eda23c413bfdc57f3252503e53
aliqyan-21 <aliqyanabid21@gmail.com>
2024-03-04 12:30:46 +0530

    changes in sigma function and tweaking in parameter

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include <cmath>
+#include <cstdio>
 #include <ctime>
-#include <iomanip>
-#include <ios>
 #include <iostream>
 
-#define Width 20
-#define Height 20
+#define Width 100
+#define Height 100
 
 // to visualize - density of pixels!
 char level[] = " .-=coaA@#";
@@ -14,20 +13,24 @@ char level[] = " .-=coaA@#";
 // initialization of parameters mentioned in the paper
 float grid[Height][Width] = {0.f};
 float grid_diff[Height][Width] = {0.f};
-float ra = 21;
+float ra = 11;
 float ri = ra / 3;
-float alpha = 0.028;
-// float alpha = 0.0147;
+float alpha_n = 0.028;
+float alpha_m = 0.147;
 float b1 = 0.278, b2 = 0.365;
 float d1 = 0.267, d2 = 0.445;
-float dt = 0.01f;
+float dt = 0.03f;
 
 inline float rand_float() { return (float)rand() / (float)RAND_MAX; }
 
 void random_grid() {
-  for (size_t i = 0; i < Height; i++) {
-    for (size_t j = 0; j < Width; j++) {
-      grid[i][j] = rand_float();
+  size_t h = Height / 2;
+  size_t w = Width / 2;
+  for (size_t i = 0; i < h; i++) {
+    for (size_t j = 0; j < w; j++) {
+      size_t x = i + Height / 2 - h / 2;
+      size_t y = j + Width / 2 - w / 2;
+      grid[x][y] = rand_float();
     }
   }
 }
@@ -36,36 +39,37 @@ void display_grid(float grid[Width][Height]) {
   for (size_t i = 0; i < Height; i++) {
     for (size_t j = 0; j < Width; j++) {
       char ch = level[(int)(grid[i][j] * ((int)level_count - 1))];
-      std::cout << ch;
+      fputc(ch, stdout);
+      fputc(ch, stdout);
     }
-    std::cout << std::endl;
+    fputc('\n', stdout);
   }
 }
 
 inline int emod(int a, int b) { return (a % b + b) % b; }
 
 // declaration of the functions mentioned in the paper
-float sigma1(float x, float a) {
+float sigma(float x, float a, float alpha) {
   return 1.f / (1.f + expf(-(x - a) * 4 / alpha));
 }
 
-float sigma2(float x, float a, float b) {
-  return sigma1(x, a) * (1 - sigma1(x, b));
+float sigma_n(float x, float a, float b) {
+  return sigma(x, a, alpha_n) * (1 - sigma(x, b, alpha_n));
 }
 
 float sigma_m(float x, float y, float m) {
-  return x * (1 - sigma1(m, 0.5f)) + y * sigma1(m, 0.5f);
+  return x * (1 - sigma(m, 0.5f, alpha_m)) + y * sigma(m, 0.5f, alpha_m);
 }
 
 // transition function mentioned in the paper
 float s(float n, float m) {
-  return sigma2(n, sigma_m(b1, d1, m), sigma_m(b2, d2, m));
+  return sigma_n(n, sigma_m(b1, d1, m), sigma_m(b2, d2, m));
 }
 
 // computing the diff of grid for each point
 void compute_grid_diff() {
-  for (int cx = 0; cx < Height; cx++) {
-    for (int cy = 0; cy < Width; cy++) {
+  for (int cy = 0; cy < Height; cy++) {
+    for (int cx = 0; cx < Width; cx++) {
       // points, and m & n
       float m = 0, M = 0;
       float n = 0, N = 0;
@@ -88,7 +92,7 @@ void compute_grid_diff() {
       n /= N;
       // normalize value of s(n, m) in range -1 to 1 instead of 0-1
       float q = s(n, m);
-      grid_diff[cx][cy] = 2 * q - 1;
+      grid_diff[cy][cx] = 2 * q - 1;
     }
   }
 }
@@ -106,7 +110,7 @@ int main() {
 
   display_grid(grid);
 
-  for (size_t it = 0; it < 10; it++) {
+  while (true) {
     compute_grid_diff();
     // apply grid diff
     for (size_t i = 0; i < Height; i++) {
